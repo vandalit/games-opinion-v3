580dae2ab9650a9aacd6cbba2c007fcb8e21e822
Van <84937597+vandalit@users.noreply.github.com>
2024-12-26 21:42:44 -0300

    paginas iniciales

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
-
+    <div id="app">
+        <Navbar />
+        <router-view />
+    </div>
 </template>
 
-<style lang="scss">
+<script>
+import Navbar from './components/AppNavbar.vue';
 
+export default {
+    name: 'App',
+    components: {
+        Navbar,
+    },
+};
+</script>
+
+<style>
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+}
 </style>
